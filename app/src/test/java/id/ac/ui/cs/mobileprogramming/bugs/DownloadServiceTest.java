2cdac2de035889b7a651f2e1954a3846523a874b
cymon1997 <cybermonster82@yahoo.com>
2018-10-12 21:06:06 +0700

    added robolectric

---
+++ app/src/test/java/id/ac/ui/cs/mobileprogramming/bugs/DownloadServiceTest.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.mobileprogramming.bugs;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import id.ac.ui.cs.mobileprogramming.bugs.service.DownloadService;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class DownloadServiceTest {
+
+    @Ignore
+    @Test
+    public void enqueueWorkTest() throws Exception {
+
+    }
+}
